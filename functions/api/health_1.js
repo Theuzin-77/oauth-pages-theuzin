d4a8ef2adee03d50d367cda997fdc3c085b4efb1
Theuzin-77 <mh.tavares.prestes@gmail.com>
2026-09-26 09:22:49 -0300

    fix: remove duplicados _1.js _2.js com SQL quebrado

--- functions/api/health_1.js
+++
@@ -1,4 +0,0 @@
-﻿import { noStoreHeaders } from '../_shared/cookies.js';
-export async function onRequest(){
-  return new Response(JSON.stringify({ ok:true, time:new Date().toISOString() }), { headers:{ 'Content-Type':'application/json', ...noStoreHeaders() } });
-}
